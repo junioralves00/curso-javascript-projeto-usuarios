2fd05ce4fb19d28592a613699707657a31e24dff
junioralves00 <junioralves00@gmail.com>
2021-01-03 17:11:09 -0300

    Aula g16

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -14,6 +14,11 @@ class UserController {
 
             event.preventDefault();
 
+            let btn = this.formEl.querySelector('[type=submit]');
+            //console.log('botao');
+
+            btn.disabled = true;
+
             let values = this.getValues();            
 
             this.getPhoto().then(
@@ -21,6 +26,10 @@ class UserController {
                     values.photo = content;
 
                     this.addLine(values);
+
+                    btn.disabled = false;
+
+                    this.formEl.reset();
                 }, (e)=>{
                     console.error(e);
             });                 
@@ -95,7 +104,7 @@ class UserController {
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
             <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
-            <td>${dataUser.birth}</td>
+            <td>${dataUser.register}</td>
             <td>
             <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
             <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
